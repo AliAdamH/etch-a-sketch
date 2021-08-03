5d664d6ae87fbc620b20e3dddbd4f8d5ad1869ce
Ali Hassaine <ali.hs@outlook.fr>
2021-08-03 22:51:13 +0000

    Add all the project's completed files

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+const boxNumber = 16;
+const container = document.querySelector('.container');
+const resetBtn = document.createElement('button');
+resetBtn.classList.add('resetBtn');
+resetBtn.textContent = 'Reset the Grid';
+document.body.insertBefore(resetBtn, container);
+//Initial Grid.
+for (let i= 0; i < boxNumber; i++) {
+    for (let j = 0; j < boxNumber; j++) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('gridbox');
+        newDiv.addEventListener('mouseover', changeColor);
+        container.appendChild(newDiv);
+    }
+}
+function changeColor (e) {
+    e.target.classList.add('boxhover');
+}
+function resetGrid () {
+    const boxlist = document.querySelectorAll('.gridbox');
+    boxlist.forEach(box => box.remove());
+    getUserInput();
+
+}
+resetBtn.addEventListener('click', resetGrid);
+
+function getUserInput() {
+    const userChoice = +prompt('Enter a number: ')
+    if(userChoice < 100) {
+        customGrid(userChoice);
+    } else {
+        alert("You've entered a number higher than 100");
+    }
+}
+function customGrid(count) {
+    container.style.cssText = `grid-template: repeat(${count}, 1fr) / repeat(${count}, 1fr)`;
+    for (let i= 0; i < count; i++) {
+        for (let j = 0; j < count; j++) {
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('gridbox');
+            newDiv.addEventListener('mouseover', changeColor);
+            container.appendChild(newDiv);
+        }
+    }
+}
+
