f14e3817a416fe9618dc39abf88cb4f0dba1b5a0
Ali Hassaine <83549605+AliAdamH@users.noreply.github.com>
2024-08-18 20:57:46 +0000

    refactor: remove duplicate grid creation logic

--- scripts/script.js
+++ scripts/script.js
@@ -5,43 +5,35 @@ resetBtn.classList.add('resetBtn');
 resetBtn.textContent = 'Reset the Grid';
 document.body.insertBefore(resetBtn, container);
 //Initial Grid.
-for (let i= 0; i < boxNumber; i++) {
-    for (let j = 0; j < boxNumber; j++) {
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('gridbox');
-        newDiv.addEventListener('mouseover', changeColor);
-        container.appendChild(newDiv);
-    }
-}
-function changeColor (e) {
-    let randomColorHue =  Math.floor(Math.random() * 360) + 1;
-    e.target.style.backgroundColor = `hsl(${randomColorHue}, 100%, 50%)`;
-}
-function resetGrid () {
-    const boxlist = document.querySelectorAll('.gridbox');
-    boxlist.forEach(box => box.remove());
-    getUserInput();
+createGrid(boxNumber);
 
+function changeColor(e) {
+  let randomColorHue = Math.floor(Math.random() * 360) + 1;
+  e.target.style.backgroundColor = `hsl(${randomColorHue}, 100%, 50%)`;
+}
+function resetGrid() {
+  const boxlist = document.querySelectorAll('.gridbox');
+  boxlist.forEach((box) => box.remove());
+  getUserInput();
 }
 resetBtn.addEventListener('click', resetGrid);
 
 function getUserInput() {
-    const userChoice = +prompt('Enter a number: ')
-    if(userChoice < 100) {
-        customGrid(userChoice);
-    } else {
-        alert("You've entered a number higher than 100");
-    }
+  const userChoice = +prompt('Enter a number: ');
+  if (userChoice < 100) {
+    customGrid(userChoice);
+  } else {
+    alert("You've entered a number higher than 100");
+  }
 }
-function customGrid(count) {
-    container.style.cssText = `grid-template: repeat(${count}, 1fr) / repeat(${count}, 1fr)`;
-    for (let i= 0; i < count; i++) {
-        for (let j = 0; j < count; j++) {
-            let newDiv = document.createElement('div');
-            newDiv.classList.add('gridbox');
-            newDiv.addEventListener('mouseover', changeColor);
-            container.appendChild(newDiv);
-        }
+function createGrid(count) {
+  container.style.cssText = `grid-template: repeat(${count}, 1fr) / repeat(${count}, 1fr)`;
+  for (let i = 0; i < count; i++) {
+    for (let j = 0; j < count; j++) {
+      let newDiv = document.createElement('div');
+      newDiv.classList.add('gridbox');
+      newDiv.addEventListener('mouseover', changeColor);
+      container.appendChild(newDiv);
     }
+  }
 }
-
